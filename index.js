ed2842825dad3df3f84642a14ba3f80c3e81fcd0
Farhan Elias <faarhaan10@gmail.com>
2022-02-10 01:27:57 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middle weares
+app.use(cors());
+app.use(express.json());
+
+
+
+//default api's
+app.get('/', (req, res) => {
+    res.send('Databse is live');
+});
+
+app.listen(port, () => {
+    console.log('DB is running on port', port);
+});
