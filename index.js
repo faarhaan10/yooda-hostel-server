5e9a589b8009afdf99ccb2afee21489a2b46a6bc
Farhan Elias <faarhaan10@gmail.com>
2022-02-10 12:16:12 +0600

    database connected

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongo cluster setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4qnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 //default api's
