d87d0ee9972fa8a2034391ff39af8339af670a38
Farhan Elias <faarhaan10@gmail.com>
2022-02-10 12:19:53 +0600

    asyns func added

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ app.use(express.json());
 //mongo cluster setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4qnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 //default api's
